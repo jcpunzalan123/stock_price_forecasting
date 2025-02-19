e45901aa8570fb34a334b7fadc0d03961c05c6ac
jcpunzalan123 <punzalanjaycee24@gmail.com>
2025-02-20 08:22:56 +0900

    Added predict function and requirements.txt file

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import uvicorn
 
 app = FastAPI()
 
+# pydantic models
+class StockIn(BaseModel):
+    ticker: str
+
+class StockOut(StockIn):
+    forecast: dict
 
 
-@app.get("/ping")
-def pong():
-    return {"ping": "pong!"}
+
+# route
+@app.post("/predict", response_model=StockOut, status_code=200)
+def get_prediction(payload: StockIn):
+    ticker = payload.ticker
+
+    prediction_list = predict(ticker)
+
+    if not prediction_list:
+        raise HTTPException(status_code=400, detail="Model not found.")
+
+    response_object = {"ticker": ticker, "forecast": convert(prediction_list)}
+    return response_object
 
 
 if __name__ == "__main__":
